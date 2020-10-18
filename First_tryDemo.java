339c2c131c5651ea92ed7e4dcdc34eaae0e2280e
Sudhaperumalsamy <sudha.perumalsamy@yahoo.com>
2020-10-18 23:03:25 +0530

    first commit

---
+++ First_tryDemo.java
@@ -0,0 +1,19 @@
+	
+public class First_tryDemo {
+	static int n=100;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		add(n);
+		
+
+	}
+
+	public static void add(int n) {
+		
+		n=80;
+		System.out.println("n:"+n);
+		
+	}
+
+}
